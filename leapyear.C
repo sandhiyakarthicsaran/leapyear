3ee4636960cc6b39df8c5168bb55947be6bc269b
sandhiyakarthicsaran <35221190+sandhiyakarthicsaran@users.noreply.github.com>
2018-01-17 16:45:15 +0530

    leapyear.C
    leapyear

---
+++ leapyear.C
@@ -0,0 +1,22 @@
+include<stdio.h>
+int main()
+{
+int year;
+printf("Enter the year:");
+scanf("%d",&year);
+if(year%4==0)
+{
+if(year%100==0)
+{
+if(year%400==0)
+printf("The year is leap");
+else
+printf("The year is not leap");
+}
+else
+printf("The year is leap year");
+}
+else
+printf("The year is not leap year");
+return 0;
+}
